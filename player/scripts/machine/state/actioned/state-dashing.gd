f51f1c3664fe7c0e995772ba8f6f6aed67c42cde
zaftnotameni <mozair.carmo+zaft@gmail.com>
2024-07-28 11:07:35 +0200

    dashparty

--- player/scripts/machine/state/actioned/state-dashing.gd
+++ player/scripts/machine/state/actioned/state-dashing.gd
@@ -2,14 +2,17 @@ class_name Z_PlayerStateDashing extends Z_PlayerStateActioned
 
 @onready var windrose : Z_PlayerWindrose = Z_ComponentBase.resolve_from(owner, Z_PlayerWindrose)
 @onready var player : Z_PlayerCharacter = owner
+@onready var party_dash_trail : CPUParticles2D = %PartyDashTrail
 
 var elapsed : float = 0.0
 
 func on_state_exit(_prev:Z_StateMachineState):
+  party_dash_trail.emitting = false
   character.viz.skew = 0
   elapsed = 0.0
 
 func on_state_enter(_prev:Z_StateMachineState):
+  party_dash_trail.emitting = true
   __zaft.audio.director.play_pitched_2d(sfx_dash, 1, true, false)
   match character.stats.facing:
     Z_PlayerStats.FACING.Right: character.viz.skew = deg_to_rad(-10)
